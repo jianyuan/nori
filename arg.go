6fd6dbc8b73912c349cc903846e98dfb23f4436f
Jian Yuan Lee <jianyuan@gmail.com>
2015-05-17 20:03:59 +0100

    Custom JSON marshal/unmarshal for argmap

--- arg.go
+++ arg.go
@@ -1,6 +1,10 @@
 package nori
 
-import "golang.org/x/net/context"
+import (
+	"encoding/json"
+
+	"golang.org/x/net/context"
+)
 
 type (
 	ArgMap map[int]interface{}
@@ -15,4 +19,38 @@ func ArgsFromContext(ctx context.Context) (ArgMap, bool) {
 
 func WithArgs(parent context.Context, val ArgMap) context.Context {
 	return context.WithValue(parent, argMapContextKey{}, val)
+}
+
+func (m ArgMap) MarshalJSON() ([]byte, error) {
+	maxKey := 0
+	for i, _ := range m {
+		if i > maxKey {
+			maxKey = i
+		}
+	}
+
+	argList := make([]interface{}, maxKey+1)
+	for i, val := range m {
+		argList[i] = val
+	}
+
+	return json.Marshal(argList)
+}
+
+func (m *ArgMap) UnmarshalJSON(b []byte) error {
+	var argList []interface{}
+	if err := json.Unmarshal(b, &argList); err != nil {
+		return err
+	}
+
+	// Check if map is initialized
+	if *m == nil {
+		*m = make(ArgMap, 0)
+	}
+
+	for i, val := range argList {
+		(*m)[i] = val
+	}
+
+	return nil
 }
--- task.go
+++ task.go
@@ -7,19 +7,19 @@ import (
 )
 
 type ProtocolV1 struct {
-	Name       string                 `json:"task"`
-	ID         string                 `json:"id"`
-	Args       []interface{}          `json:"args,omitempty"`
-	KWArgs     map[string]interface{} `json:"kwargs,omitempty"`
-	Retries    int                    `json:"retries,omitempty"`
-	ETA        *time.Time             `json:"eta,omitempty"`
-	Expires    *time.Time             `json:"expires,omitempty"`
-	UTC        bool                   `json:"utc,omitempty"`
-	Callbacks  []string               `json:"callbacks,omitempty"`
-	Errbacks   []string               `json:"errbacks,omitempty"`
-	TimeLimits [2]*float64            `json:"timelimit,omitempty"`
-	TaskSet    *string                `json:"taskset,omitempty"`
-	Chord      *string                `json:"chord,omitempty"`
+	Name       string      `json:"task"`
+	ID         string      `json:"id"`
+	Args       ArgMap      `json:"args,omitempty"`
+	KWArgs     KWArgMap    `json:"kwargs,omitempty"`
+	Retries    int         `json:"retries,omitempty"`
+	ETA        *time.Time  `json:"eta,omitempty"`
+	Expires    *time.Time  `json:"expires,omitempty"`
+	UTC        bool        `json:"utc,omitempty"`
+	Callbacks  []string    `json:"callbacks,omitempty"`
+	Errbacks   []string    `json:"errbacks,omitempty"`
+	TimeLimits [2]*float64 `json:"timelimit,omitempty"`
+	TaskSet    *string     `json:"taskset,omitempty"`
+	Chord      *string     `json:"chord,omitempty"`
 }
 
 type TaskFunc func(context.Context) error
