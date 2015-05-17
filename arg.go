7cf0c7a6918eca0918fdf677626c21c673f45b34
Jian Yuan Lee <jianyuan@gmail.com>
2015-05-17 20:07:51 +0100

    Add comments to args

--- arg.go
+++ arg.go
@@ -7,23 +7,28 @@ import (
 )
 
 type (
+	// ArgMap represents the positional arguments of a task
 	ArgMap map[int]interface{}
 
+	// argMapContextKey holds the context key of an ArgMap
 	argMapContextKey struct{}
 )
 
+// ArgsFromContext extracts the ArgMap from context
 func ArgsFromContext(ctx context.Context) (ArgMap, bool) {
 	val, ok := ctx.Value(argMapContextKey{}).(ArgMap)
 	return val, ok
 }
 
+// WithArgs returns a new context with the ArgMap
 func WithArgs(parent context.Context, val ArgMap) context.Context {
 	return context.WithValue(parent, argMapContextKey{}, val)
 }
 
+// MarshalJSON marshals ArgMap into a JSON array
 func (m ArgMap) MarshalJSON() ([]byte, error) {
 	maxKey := 0
-	for i, _ := range m {
+	for i := range m {
 		if i > maxKey {
 			maxKey = i
 		}
@@ -37,6 +42,7 @@ func (m ArgMap) MarshalJSON() ([]byte, error) {
 	return json.Marshal(argList)
 }
 
+// UnmarshalJSON unmarshals JSON array into ArgMap
 func (m *ArgMap) UnmarshalJSON(b []byte) error {
 	var argList []interface{}
 	if err := json.Unmarshal(b, &argList); err != nil {
--- kwarg.go
+++ kwarg.go
@@ -3,16 +3,20 @@ package nori
 import "golang.org/x/net/context"
 
 type (
+	// KWArgMap represents the keyword arguments of a task
 	KWArgMap map[string]interface{}
 
+	// kwargMapContextKey holds the context key of a KWArgMap
 	kwargMapContextKey struct{}
 )
 
+// KWArgsFromContext extracts the KWArgMap from context
 func KWArgsFromContext(ctx context.Context) (KWArgMap, bool) {
 	val, ok := ctx.Value(kwargMapContextKey{}).(KWArgMap)
 	return val, ok
 }
 
+// WithKWArgs returns a new context with the KWArgMap
 func WithKWArgs(parent context.Context, val KWArgMap) context.Context {
 	return context.WithValue(parent, kwargMapContextKey{}, val)
 }
