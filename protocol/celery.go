6a05eff6db7cbbddd0821e70d08e17b049675950
Jian Yuan Lee <jianyuan@gmail.com>
2016-03-20 18:39:21 +0000

    Remove arg and kwarg structs

--- protocol/celery.go
+++ protocol/celery.go
@@ -3,19 +3,19 @@ package protocol
 import "time"
 
 type CeleryTask struct {
-	Name       string      `json:"task"`
-	ID         string      `json:"id"`
-	Args       ArgMap      `json:"args,omitempty"`
-	KWArgs     KWArgMap    `json:"kwargs,omitempty"`
-	Retries    int         `json:"retries,omitempty"`
-	ETA        *time.Time  `json:"eta,omitempty"`
-	Expires    *time.Time  `json:"expires,omitempty"`
-	UTC        bool        `json:"utc,omitempty"`
-	Callbacks  []string    `json:"callbacks,omitempty"`
-	Errbacks   []string    `json:"errbacks,omitempty"`
-	TimeLimits [2]*float64 `json:"timelimit,omitempty"`
-	TaskSet    *string     `json:"taskset,omitempty"`
-	Chord      *string     `json:"chord,omitempty"`
+	Name       string                 `json:"task"`
+	ID         string                 `json:"id"`
+	Args       []interface{}          `json:"args,omitempty"`
+	KWArgs     map[string]interface{} `json:"kwargs,omitempty"`
+	Retries    int                    `json:"retries,omitempty"`
+	ETA        *time.Time             `json:"eta,omitempty"`
+	Expires    *time.Time             `json:"expires,omitempty"`
+	UTC        bool                   `json:"utc,omitempty"`
+	Callbacks  []string               `json:"callbacks,omitempty"`
+	Errbacks   []string               `json:"errbacks,omitempty"`
+	TimeLimits [2]*float64            `json:"timelimit,omitempty"`
+	TaskSet    *string                `json:"taskset,omitempty"`
+	Chord      *string                `json:"chord,omitempty"`
 }
 
 type CeleryResult struct {
